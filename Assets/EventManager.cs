545ac26bce88242d9520d25797203be0e2dba627
Илья Галкин <galkincmr@gmail.com>
2023-02-03 19:27:27 +0300

    add_event

--- Assets/EventManager.cs
+++ Assets/EventManager.cs
@@ -15,12 +15,17 @@ public class EventManager : MonoBehaviour
     public GameObject eventRewardPopUp;
 
     public GameObject[] events = new GameObject[7];
+    public GameObject[] eventsUnlocked = new GameObject[7];
     public Text[] rewardText = new Text[7];
     public Text[] currencyText = new Text[7];
     public Text[] startText = new Text[7];
-
+    public Text[] costText = new Text[7];
+    
     public BigDouble[] reward = new BigDouble[7];
     public BigDouble[] currencies = new BigDouble[7];
+    public BigDouble[] cost = new BigDouble[7];
+
+    public int[] levels = new int[7];
 
     public bool eventActive;
     public int eventActiveID;
@@ -44,6 +49,48 @@ public class EventManager : MonoBehaviour
         eventTokensText.text = $"Event Tokens: {game.NotationMethod(data.eventTokens, "F2")} ({game.NotationMethod(eventTokenBoost, "F2")})";
         reward[0] = BigDouble.Log10(currencies[0] + 1);
         reward[1] = BigDouble.Log10(currencies[1] / 5  + 1);
+
+        for (int i = 0; i < 2; i++)
+            cost[i] = 10 * BigDouble.Pow(1.15, levels[i]);
+
+        if(previousDayChecked != DayOfTheWeek() & eventActive)
+        {
+            eventActiveID = 0;
+            for (var i = 0; i < 2; i++)
+                data.eventCooldown[i] = 0;
+        }
+
+        switch(DayOfTheWeek())
+        {
+            case "Monday":
+                RunEventUI(1);
+                break;
+            case "Tuesday":
+                RunEventUI(2);
+                break;
+        }
+
+        previousDayChecked = DayOfTheWeek();
+
+    }
+    private void RunEventUI(int id)
+    {
+        var data = game.data;
+        for (var i = 0; i < 2; i++)
+            events[i].gameObject.SetActive(false);
+        events[id].gameObject.SetActive(true);
+
+        if (eventActiveID != id) return;
+        eventsUnlocked[id].gameObject.SetActive(true);
+        rewardText[id].text = $"+{reward[id]} Event Tokens";
+        currencyText[id].text = $"{currencies[id]} Currency";
+        costText[id].text = $"Cost: {cost[id]}";
+
+        if(eventActiveID == 0)
+        {
+            var time = TimeSpan.FromSeconds(data.eventCooldown[id]);
+            startText[id].text = data.eventCooldown[id] > 0 ? "" : "Start Event";
+        }
 
     }
 }
